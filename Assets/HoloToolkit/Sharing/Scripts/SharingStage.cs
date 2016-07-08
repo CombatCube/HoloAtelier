b573eb31596d6ae7c7d52eb079f698ca83b667aa
Jesus Ramirez <Jesus_Ramirez@gnwc.private>
2016-07-08 10:51:50 -0700

    Menu 1 (Icons) Menu 2 (Icons with Palette)

--- Assets/HoloToolkit/Sharing/Scripts/SharingStage.cs
+++ Assets/HoloToolkit/Sharing/Scripts/SharingStage.cs
@@ -13,7 +13,7 @@ namespace HoloToolkit.Sharing
         /// </summary>
         public ClientRole ClientRole = ClientRole.Primary;
 
-        private string ServerAddress = "10.159.23.125";
+        private string ServerAddress = "10.159.23.87";
         public int ServerPort = 20602;
 
         private SharingManager sharingMgr;
---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class MenuManager : MonoBehaviour {
+
+	public GameObject ActiveMenu;
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void ToggleMenu(string name)
+	{
+
+		if (name == "Menu 1") {
+			Transform menu = ActiveMenu.transform.Find (name);
+			Transform InactiveMenu = ActiveMenu.transform.Find ("Menu 2");
+			InactiveMenu.gameObject.SetActive (false);
+			menu.gameObject.SetActive(!menu.gameObject.activeSelf);
+		}
+		else
+		if (name == "Menu 2") {
+			Transform menu = ActiveMenu.transform.Find (name);
+			Transform InactiveMenu = ActiveMenu.transform.Find ("Menu 1");
+			InactiveMenu.gameObject.SetActive (false);
+			menu.gameObject.SetActive(!menu.gameObject.activeSelf);
+		}
+
+
+	}
+}
+
