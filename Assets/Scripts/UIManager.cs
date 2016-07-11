453d84fec88d80e42e284bd66b4fd0f36e7ca873
Andrew Coccimiglio <acoccimi@gmail.com>
2016-07-11 11:58:03 -0700

    Added voice commands; created prefabs for UI

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIManager : MonoBehaviour {
+    public GameObject Menu1;
+    public GameObject Menu2;
+    public GameObject Menu3;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    void OnMenu1()
+    {
+        Menu1.SetActive(true);
+        Menu2.SetActive(false);
+        Menu3.SetActive(false);
+    }
+    void OnMenu2()
+    {
+        Menu1.SetActive(false);
+        Menu2.SetActive(true);
+        Menu3.SetActive(false);
+    }
+    void OnMenu3()
+    {
+        Menu1.SetActive(false);
+        Menu2.SetActive(false);
+        Menu3.SetActive(true);
+    }
+}
